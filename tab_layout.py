ce1e1daf9589dd7cce7efe5f37518b776b6d1919
Laddie <laddietanjicheng@gmail.com>
2023-01-16 11:40:49 +0800

    change to st.cache

--- tab_layout.py
+++ tab_layout.py
@@ -26,7 +26,7 @@ extractor = URLExtract()
 openai.api_key = st.secrets['openai']  
 
 
-@st.experimental_singleton
+@st.cache
 def runModel():
     model = EncoderDecoderModel.from_pretrained("patrickvonplaten/longformer2roberta-cnn_dailymail-fp16")
     tokenizer = LongformerTokenizer.from_pretrained("allenai/longformer-base-4096")
